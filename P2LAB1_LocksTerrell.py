d88d66e4381bb25839abe8666d0c70958702ffcf
Terrell <146045781+lockst5364@users.noreply.github.com>
2023-12-10 23:06:55 -0500

    Add files via upload

---
+++ P2LAB1_LocksTerrell.py
@@ -0,0 +1,5 @@
+miles_per = float(input())
+dollars_per = float(input())
+
+print(f'{(20 * dollars_per / miles_per):.2f} {(75 * dollars_per / miles_per):.2f} {(500 * dollars_per / miles_per):.2f}')
+
